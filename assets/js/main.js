43813f91e9ea1647f256eeac65600a9230922a9d
Aaron <gauntta@gmail.com>
2019-06-10 18:22:56 -0500

    code cleanup and replace carousel images

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,39 +26,39 @@ $(document).ready(function(){
     $('.carousel').carousel();
 
     function descriptionSwitch(e) {
-    var name1 = "Tasty Burger Time";
-    var name2 = "Crystal Collector";
-    var name3 = "Whats Wrong With My Body";
-    var name4 = "Test 4";  
-    var desc1 = "A handlebars based app that allows users to add and devour various kinds of burgers. Maybe even some sandwitches if your're feeling bold.";
-    var desc2 = "A javascript logic game where four crystals are assigned random values, the user has to figure out their values and use them to reach a randomized total value.";
-    var desc3 = "A mySQL and node based patient portal for a hypothetical hospital. First time uses will be prompted to create an account, and will have a dashboard where they can access their patient records. It also includes a specialized search engine using the medicAPI to return possible diagnosis based on reported symptoms.";
-    var desc4 = "Consectetur adipiscing elit duis tristique sollicitudin nibh sit.";
+    var name1 = "IronHaus";
+    var name2 = "Whats Wrong With My Body";
+    var name3 = "Tasty Burger Time";
+    var name4 = "Crystal Collector";  
+    var desc1 = "A fitness social media and logging app using the MERN stack";
+    var desc2 = "A mySQL and node based patient portal for a hypothetical hospital. First time uses will be prompted to create an account, and will have a dashboard where they can access their patient records. It also includes a specialized search engine using the medicAPI to return possible diagnosis based on reported symptoms.";
+    var desc3 = "A handlebars based app that allows users to add and devour various kinds of burgers. Maybe even some sandwitches if your're feeling bold.";
+    var desc4 = "A javascript logic game where four crystals are assigned random values, the user has to figure out their values and use them to reach a randomized total value.";
 
     switch (e) {
       case "carousel-1": 
       $("#folio-name").text(name1);
       $("#folio-description").text(desc1);
-      $("#github-link").attr("href", "https://github.com/Agauntt/Eat-da-burger");
-      $("#deployment-link").attr("href", "https://tasty-burger-time.herokuapp.com/");
+      $("#github-link").attr("href", "https://github.com/Agauntt/IronHaus");
+      $("#deployment-link").attr("href", "https://ironhaus.herokuapp.com/");
       break;
       case "carousel-2":
       $("#folio-name").text(name2);
       $("#folio-description").text(desc2);
-      $("#github-link").attr("href", "https://github.com/https://github.com/Agauntt/unit-4-game");
-      $("#deployment-link").attr("href", "https://agauntt.github.io/unit-4-game/");
+      $("#github-link").attr("href", "https://github.com/https://github.com/Agauntt/WWWMB");
+      $("#deployment-link").attr("href", "https://agauntt.github.io/");
       break;
       case "carousel-3": 
       $("#folio-name").text(name3);
       $("#folio-description").text(desc3);
-      $("#github-link").attr("href", "https://github.com/Agauntt/Project2");
-      $("#deployment-link").attr("href", "");
+      $("#github-link").attr("href", "https://github.com/Agauntt/Eat-da-Burger");
+      $("#deployment-link").attr("href", "https://tasty-burger-time.herokuapp.com");
       break;
       case "carousel-4":
       $("#folio-name").text(name4);
       $("#folio-description").text(desc4);
-      $("#github-link").attr("href", "");
-      $("#deployment-link").attr("href", "");
+      $("#github-link").attr("href", "https://github.com/Agauntt/Crystal_Collector");
+      $("#deployment-link").attr("href", "https://agauntt.github.io/Crystal_Collector");
       break;
     }
   }
